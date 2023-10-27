3ff941850b0ce9944c5bc05d98d3ab3362bdb6bd
kjoedicker <kjoedicker@gmail.com>
2023-10-26 22:24:42 -0400

    feat: allow for programs to be configured

--- all-for-one.py
+++ all-for-one.py
@@ -1,18 +1,8 @@
 import argparse
 import sys
 import os
-
-
-# TODO: switch this over to a config?
-programs = {
-    'gri': 'python3 ../gri/main.py',
-    'pker': 'perl ../pker/pker.pl',
-    'watchkeeper': 'go run ../watchkeeper/main.go',
-    'git-delorean': 'python3 ../git-delorean/main.py',
-    'git-latest': 'perl ../git-latest/git-latest.pl',
-    'readme-init': 'python3 ../readme-init.py',
-    'jira': 'node ../private/jira-fetch/jira.js'
-}
+import yaml
+import os.path
 
 def parseCliArguments() -> (str, str):
     if (len(sys.argv) < 2):
@@ -22,15 +12,30 @@ def parseCliArguments() -> (str, str):
     SUBCOMMAND_INDEX = 2
 
     programName = sys.argv[PROGRAM_INDEX]
-    if programName not in programs:
+    subcommands = sys.argv[SUBCOMMAND_INDEX:]
+
+    return (programName, subcommands)
+
+def readConfig():
+    dirname = os.path.dirname(__file__) or '.'
+    with open(dirname + '/programs.yml', 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+    
+def buildCommand(programName: str, subCommands: str):
+    config = readConfig()
+
+    if programName not in config['programs']:
         sys.exit(f"Program '{programName}' is not configured.")
 
-    program = programs[programName]
-    subcommands = " ".join(sys.argv[SUBCOMMAND_INDEX:]) if len(sys.argv) > 2 else ""
+    rootPath = config['rootPath']
+    program = config['programs'][programName]
+    subCommands = " ".join(subCommands)
 
-    return (program, subcommands)
+    return f"{program['runtime']} {rootPath}{program['path']} {subcommands}"
 
 if __name__ == '__main__':
-    (program, subcommands) = parseCliArguments()
-    command = f"{program} {subcommands}"
+    (programName, subcommands) = parseCliArguments()
+    command = buildCommand(programName, subcommands)
+
     os.system(command)
